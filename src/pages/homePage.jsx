9f3ff87b1299dfb54b7a319488c90f7db7098c63
yonal5 <yonalabasingha@gmail.com>
2026-01-15 09:00:25 +0530

    Update homePage.jsx

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -56,7 +56,7 @@ export default function HomePage() {
     {/* ================= HEADER ================= */}
     <div className="text-center">
       <h2 className="text-3xl md:text-4xl font-bold text-gray-900">
-        Build, Launch & Sell Your Website With Confidence
+        Build, Launch & Get Your Website With Confidence Divolopers
       </h2>
 
       <p className="mt-6 text-gray-600 text-lg max-w-4xl mx-auto">
